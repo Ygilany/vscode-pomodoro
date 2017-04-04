ce476b4b741bacd281e6e3b9af83e77c69047eb0
Yahya Gilany <yahya.gilany@live.com>
2017-04-04 18:14:50 -0400

    preload the pomodoro

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,16 @@ import * as vscode from 'vscode';
 import { Pomodoro } from './pomodoro';
 import { StatusBar } from './ui';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {	
 	const pomodoro = Pomodoro.getInstance();
+	const statusBars = StatusBar.getInstance();
+
+	pomodoro.preload();
 	
+	statusBars.updateTasksCounter(pomodoro.currentTaskIndex, pomodoro.tasks.length);
 
 	vscode.commands.registerCommand(`pomodoro.addTask`, () => pomodoro.addTask());
-	vscode.commands.registerCommand(`pomodoro.run`, () => pomodoro.run());
-	
+	vscode.commands.registerCommand(`pomodoro.run`, () => pomodoro.run());	
 }
 
 export function deactivate() {}
