94ca4536bc9a74970d377189bd64242c0604894a
Yahya Gilany <yahya.gilany@live.com>
2017-04-04 23:47:54 -0400

    get the default tasks.json file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import { workspace } from 'vscode';
 import { TimeUnits } from './timer';
 
+import path = require('path');
+
 const DEFAULT_TASK_DURATION = 25 * TimeUnits.Minute;
 const DEFAULT_BREAK_DURATION = 5 * TimeUnits.Minute;
 const DEFAULT_LONG_BREAK_DURATION = 15 * TimeUnits.Minute;
@@ -18,15 +20,14 @@ export interface Config {
 export function getConfig(): Config {
     let configuration;
     try {
-    configuration = workspace.getConfiguration('pomodoro');
-
-    } catch (error) {}
-
+        configuration = workspace.getConfiguration('pomodoro');
+    } catch (error) {}    
+    
     return {
       task_duration: configuration.task_durarion * TimeUnits.Minute || DEFAULT_TASK_DURATION,
       break_duration: configuration.break_duration * TimeUnits.Minute || DEFAULT_BREAK_DURATION,
       long_break_duration: configuration.long_break_duration * TimeUnits.Minute || DEFAULT_LONG_BREAK_DURATION,
       counter_to_long_break: configuration.counter_to_long_break || COUNTER_TO_LONG_BREAK, 
-      tasks_file: configuration.task_file || `/Users/yahyagilany/Documents/projects/vscode-pomodoro/tasks.json`
+      tasks_file: configuration.task_file || path.join(__dirname, `../../tasks.json`)
     } as Config;
 }
